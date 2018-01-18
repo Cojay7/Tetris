431d717dfb6628bbca8d6d1794ff3678751eac1c
Unknown <hustache.julie@orange.fr>
2018-01-18 15:57:23 +0100

    Auto stash before merge of "Dev" and "origin/Dev"

--- tetris-dao/src/main/java/fr/formation/tetris_dao/config/ConfigSpring.java
+++ tetris-dao/src/main/java/fr/formation/tetris_dao/config/ConfigSpring.java
@@ -2,17 +2,68 @@ package fr.formation.tetris_dao.config;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan({"fr.formation.tetrimino_model","fr.formation.tetris_dao.tetrimino.dao"})
 @PropertySource({ "classpath:data-source.properties" })
 @EnableAspectJAutoProxy
+@EnableTransactionManagement
+//Pensez à mettre à jour selon les besoins
+@EnableJpaRepositories({"fr.formation.tetrimino_model","fr.formation.tetris","fr.formation.tetris_model_faq"})
 public class ConfigSpring {
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(BasicDataSource dataSource) {
+	LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+	emf.setDataSource(dataSource);
+	//Pensez à mettre à jour selon les besoins
+	emf.setPackagesToScan("fr.formation.tetrimino_model","fr.formation.tetris","fr.formation.tetris_model_faq");
+	JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	emf.setJpaVendorAdapter(vendorAdapter);
+	emf.setJpaProperties(this.hibernateProperties());
+	return emf;
+	}
+	
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.format_sql", "true");
+		properties.setProperty("hibernate.cache.use_query_cache", "true"); 
+		properties.setProperty("hibernate.cache.use_second_level_cache", "true");
+		properties.setProperty("hibernate.cache.region.factory_class",
+				"org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory");
+		
+		return properties;
+		}
+	
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+	JpaTransactionManager transactionManager = new JpaTransactionManager();
+	transactionManager.setEntityManagerFactory(emf);
+	return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+	return new PersistenceExceptionTranslationPostProcessor();
+	}
 	@Autowired
 	private Environment env;
 	
