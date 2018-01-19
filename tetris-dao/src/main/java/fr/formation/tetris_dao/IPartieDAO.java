475c6e0eafc9973d3544b00ce309238ab34f9e79
Unknown <hustache.julie@orange.fr>
2018-01-19 16:52:09 +0100

    Dernière modif-Julie

--- tetris-dao/src/main/java/fr/formation/tetris_dao/IPartieDAO.java
+++ tetris-dao/src/main/java/fr/formation/tetris_dao/IPartieDAO.java
@@ -1,9 +1,14 @@
 package fr.formation.tetris_dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import fr.formation.partie.Coup;
 import fr.formation.partie.Partie;
+import fr.formation.utilisateur.Joueur;
 
 public interface IPartieDAO extends JpaRepository<Partie, Integer>{
 
+	public List<Partie> findByJoueur(Joueur joueur);
 }
